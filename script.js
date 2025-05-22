2fd062c58d71a4461bc44ad803ac7832a4a1e02d
naimanasifa12 <naimanasifa@gmail.com>
2025-05-22 07:35:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Jika ingin menggunakan animasi ketik manual, pakai ini:
+// (Opsional, karena CSS sudah cukup untuk efek ketik)
+
+const text = "I'm a Web Developer";
+const target = document.getElementById("typing-text");
+
+target.innerHTML = text;
